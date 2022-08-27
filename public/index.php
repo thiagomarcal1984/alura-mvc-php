c571ccdadd94700ed730824f15076d6227d0d316
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-27 17:30:09 -0300

    Formulários e PHP.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
 
 $caminho = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : $_SERVER['REQUEST_URI'];
 
@@ -15,6 +16,11 @@ switch ($caminho) {
         
     case '/novo-curso':
         $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+        
+    case '/salvar-curso':
+        $controlador = new Persistencia();
         $controlador->processaRequisicao();
         break;
         
