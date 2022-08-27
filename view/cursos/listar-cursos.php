96b0a9314a4814b9a7cb30c05da301bfb56b2796
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-27 16:53:14 -0300

    Particionando o HTML.

--- view/cursos/listar-cursos.php
+++ view/cursos/listar-cursos.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <div class="jumbotron">
-        <h1>Listar cursos</h1>
-    </div>
-    
-    <a href="/novo-curso" class="btn btn-primary mb-2">Novo curso</a>
-    
+<?php include __DIR__ . '/../inicio-html.php'; ?>
+<a href="/novo-curso" class="btn btn-primary mb-2">Novo curso</a>
+
     <ul class="list-group">
         <?php foreach ($cursos as $curso): ?>
             <li class="list-group-item">
@@ -20,6 +8,4 @@
             </li>
         <?php endforeach; ?>
     </ul>
-</div>
-</body>
-</html>
+<?php include __DIR__ . '/../fim-html.php'; ?>
