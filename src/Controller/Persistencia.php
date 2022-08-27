6a15f3523f238d3c722d02dd6393555c2ccddb53
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-08-27 17:45:17 -0300

    Filtrando dados.

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -18,8 +18,13 @@ class Persistencia implements InterfaceControladorRequisicao
 
     public function processaRequisicao(): void
     {
+        $descricao = filter_input(
+            INPUT_POST, 
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
         $curso = new Curso();
-        $curso->setDescricao($_REQUEST['descricao']);
+        $curso->setDescricao($descricao);
         $this->entityManager->persist($curso);
         $this->entityManager->flush();
     }
